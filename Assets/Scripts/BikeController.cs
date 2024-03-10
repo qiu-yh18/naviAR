8b2bdbbe3cc767bf3b30a08fc76f6049a0896868
Christine Qiu <cqiu@kth.se>
2024-03-10 17:09:34 +0100

    Disable useless objects

--- Assets/Scripts/BikeController.cs
+++ Assets/Scripts/BikeController.cs
@@ -5,10 +5,10 @@ public class BikeController : MonoBehaviour
     // public float speed = 3f;
     // public float rotationSpeed = 80f;
     public GameObject environment;
-    public OVRCameraRig ovrCameraRig;
+    // public OVRCameraRig ovrCameraRig;
     public GameObject trackingSpace;
-    private Vector3 oldCameraPos;
     private Vector3 oldTrackingSpacePos;
+    private Vector3 movement;
     private Vector3 bike2CameraPositionOffset;
     private Vector3 bike2CameraRotationOffset;
     private bool isOldPos = false;
@@ -25,10 +25,8 @@ public class BikeController : MonoBehaviour
 
     private void Awake(){
         // Calculate the initial offset between the bike and the OVRCameraRig
-        bike2CameraPositionOffset = transform.position - ovrCameraRig.transform.position;
-        bike2CameraRotationOffset = transform.eulerAngles - ovrCameraRig.transform.eulerAngles;
-        oldCameraPos = ovrCameraRig.transform.position;
-        // Debug.Log(oldCameraPos);
+        bike2CameraPositionOffset = transform.position - trackingSpace.transform.position;
+        bike2CameraRotationOffset = transform.eulerAngles - trackingSpace.transform.eulerAngles;
         oldTrackingSpacePos = trackingSpace.transform.position;
         Debug.Log("!!!!!!");
         Debug.Log(oldTrackingSpacePos);
@@ -36,50 +34,48 @@ public class BikeController : MonoBehaviour
 
     private void Start(){
         // Calculate the initial offset between the bike and the OVRCameraRig
-        bike2CameraPositionOffset = transform.position - ovrCameraRig.transform.position;
-        bike2CameraRotationOffset = transform.eulerAngles - ovrCameraRig.transform.eulerAngles;
-        oldCameraPos = ovrCameraRig.transform.position;
-        // Debug.Log(oldCameraPos);
+        bike2CameraPositionOffset = transform.position - trackingSpace.transform.position;
+        bike2CameraRotationOffset = transform.eulerAngles - trackingSpace.transform.eulerAngles;
         oldTrackingSpacePos = trackingSpace.transform.position;
         Debug.Log("@@@@");
         Debug.Log(oldTrackingSpacePos);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         // Update the bike's position based on the calculated offset
-        if (ovrCameraRig != null)
+        if (trackingSpace != null)
         {
-            // if(!isOldPos || !oldCameraPos || ovrCameraRig.transform.position != oldCameraPos){
-            //     isOldPos = true;
-            //     oldCameraPos = ovrCameraRig.transform.position;
-            //     Debug.Log(oldCameraPos);
-            // }
-            if(!isOldPos || oldTrackingSpacePos==new Vector3(0f,0f,0f) || Vector3.Distance(trackingSpace.transform.position, oldTrackingSpacePos) > movementThreshold){
+            // Calculate user movement between frames
+            if(trackingSpace != null && oldTrackingSpacePos != new Vector3(0f,0f,0f)){
+                movement = trackingSpace.transform.position - oldTrackingSpacePos;
+                movement[2] = 0.0f;
+            }
+            if(!isOldPos || oldTrackingSpacePos == new Vector3(0f,0f,0f) || movement.magnitude > movementThreshold){
                 isOldPos = true;
                 oldTrackingSpacePos = trackingSpace.transform.position;
                 Debug.Log("??????");
                 Debug.Log(oldTrackingSpacePos);
             }
             // Calculate the updated position offset
-            Vector3 updatedPositionOffset = ovrCameraRig.transform.TransformVector(bike2CameraPositionOffset);
-            transform.position = ovrCameraRig.transform.position + updatedPositionOffset;
+            Vector3 updatedPositionOffset = trackingSpace.transform.TransformVector(bike2CameraPositionOffset);
+            transform.position = trackingSpace.transform.position + updatedPositionOffset;
 
             // Calculate the updated rotation offset
-            Quaternion updatedRotation = ovrCameraRig.transform.rotation * Quaternion.Euler(bike2CameraRotationOffset);
+            Quaternion updatedRotation = trackingSpace.transform.rotation * Quaternion.Euler(bike2CameraRotationOffset);
             transform.rotation = updatedRotation;
         }
 
         // Move the city in the opposite direction of the bike's movement
-        // if (environment != null)
-        // {
-        //     if(angleGain>0){
+        if (environment != null)
+        {
+            if(angleGain > 0){
         //         environment.transform.RotateAround(transform.position, Vector3.up, -angleGain * horizontalInput * rotationSpeed * Time.deltaTime);
         //         environment.transform.Rotate(Vector3.up, -0.2f*angleGain * speedGain * speed * Time.deltaTime);
-        //     }
-        //     if(speedGain>0){
-        //         environment.transform.Translate(-speedGain*transform.forward * speed * Time.deltaTime, Space.World);
-        //     }
-        // }
+            }
+            if(speedGain>0){
+                environment.transform.Translate(-speedGain * movement * Time.deltaTime, Space.World);
+            }
+        }
     }
 }
