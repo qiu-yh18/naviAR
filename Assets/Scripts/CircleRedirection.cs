8ddf525f017521c89c204eeba59efeb30a66462b
Christine Qiu <cqiu@kth.se>
2024-03-21 17:06:08 +0100

    Try dot product

--- Assets/Scripts/CircleRedirection.cs
+++ Assets/Scripts/CircleRedirection.cs
@@ -5,17 +5,18 @@ public class CircleRedirection : MonoBehaviour
     // Adjust this parameter to control the curvature gain effect
     public float alpha = 5.0f;
     public Transform playerTransform;
-    private Vector3 previousPlayerPosition;
-    // private Vector3 startingPoint;
+    private Vector3 previousPlayerPositionXZ;
+    private Vector3 startPositionXZ;
+    public Vector3 circleOriginPosition;
 
     void Start()
     {
         // Initialize the previous player position
-        previousPlayerPosition = playerTransform.position;
-        // startingPoint = playerTransform.position;
+        startPositionXZ = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
+        previousPlayerPositionXZ = startPositionXZ;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // Transform player's position into environment's local coordinate system
         // Vector3 playerLocalPosition = transform.InverseTransformPoint(playerTransform.position);
@@ -24,15 +25,18 @@ public class CircleRedirection : MonoBehaviour
         // Vector3 displacement = playerLocalPosition - transform.InverseTransformPoint(previousPlayerPosition);
         // Debug.Log(displacement);
 
-        Vector3 displacement = playerTransform.position - previousPlayerPosition;
-
+        Vector3 displacementXZ = new Vector3(playerTransform.position.x, 0, playerTransform.position.z) - previousPlayerPositionXZ;
+        Vector3 toOrigin = circleOriginPosition - previousPlayerPositionXZ;
+        float product = Vector3.Dot(displacementXZ.normalized, toOrigin.normalized);
+        int direction = (int)Mathf.Sign(product);
+        float rotationAngle = displacementXZ.magnitude * alpha * direction;
         // Calculate the rotation angle based on the magnitude of the displacement vector
-        float rotationAngle = displacement.magnitude * alpha;
+        // float rotationAngle = displacementXZ.magnitude * alpha;
         
         // Apply rotation to the environment around the starting point
         transform.RotateAround(playerTransform.position, Vector3.up, -rotationAngle); // Negative rotation for environment
 
         // Update the previous player position
-        previousPlayerPosition = playerTransform.position;
+        previousPlayerPositionXZ = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
     }
 }
