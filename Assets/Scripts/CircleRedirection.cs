693a6ad803e39fa23df4116c8aa3cb4f39cf921d
Christine Qiu <cqiu@kth.se>
2024-03-23 18:13:35 +0100

    Try circle to line mapping

--- Assets/Scripts/CircleRedirection.cs
+++ Assets/Scripts/CircleRedirection.cs
@@ -4,27 +4,33 @@ public class CircleRedirection : MonoBehaviour
 {
     public float alpha = 5.0f;
     public Transform playerTransform;
+    private Vector3 currentPlayerPositionXZ;
     private Vector3 previousPlayerPositionXZ;
     private Vector3 startPositionXZ;
+    private Vector3 startRotationXZ;
     public Transform originTransform;
     private Vector3 circleOriginPositionXZ;
     private float radius;
+    private float xOnLine;
+    private float previousXOnLine;
     void Start()
     {
         startPositionXZ = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
         previousPlayerPositionXZ = startPositionXZ;
         circleOriginPositionXZ = new Vector3(originTransform.position.x, 0, originTransform.position.z);
-        radius = Math.abs((startPositionXZ - circleOriginPositionXZ).magnitude);
+        radius = Mathf.Abs((startPositionXZ - circleOriginPositionXZ).magnitude);
+        previousXOnLine = 0f;
     }
 
     void Update()
     {
-        Vector3 displacementXZ = new Vector3(playerTransform.position.x, 0, playerTransform.position.z) - previousPlayerPositionXZ;
-        Vector3 toOrigin = circleOriginPositionXZ - previousPlayerPositionXZ;
-        float product = Vector3.Dot(displacementXZ.normalized, toOrigin.normalized);
-        int direction = (int)Mathf.Sign(product);
-        float rotationAngle = displacementXZ.magnitude * alpha * direction;
+        currentPlayerPositionXZ = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
+        float theta = Mathf.Atan2(currentPlayerPositionXZ.z - circleOriginPositionXZ.z, currentPlayerPositionXZ.x - circleOriginPositionXZ.x);
+        xOnLine = radius * Mathf.Cos(theta);
+        float displacementX = xOnLine - previousXOnLine;
+        float rotationAngle = Mathf.Atan2(displacementX, radius) * Mathf.Rad2Deg * alpha;
         transform.RotateAround(playerTransform.position, Vector3.up, -rotationAngle);
-        previousPlayerPositionXZ = new Vector3(playerTransform.position.x, 0, playerTransform.position.z);
+        previousPlayerPositionXZ = currentPlayerPositionXZ;
+        previousXOnLine = xOnLine;
     }
 }
