fe72f1b5173d3993786ff37dad89dd0f171c289e
Christine Qiu <cqiu@kth.se>
2024-04-01 16:20:01 +0200

    Change map

--- Assets/Scripts/CalibrateManager.cs
+++ Assets/Scripts/CalibrateManager.cs
@@ -56,7 +56,7 @@ public class CalibrateManager : MonoBehaviour
                         // Stage 1: walk to the center, place left controller on the ground, click set circle center
                         if (!isCircleCenterSet)
                         {
-                            circleCenterToCalibrate.transform.position = new Vector3(controllerForCalibration.transform.position.x, 0f, controllerForCalibration.transform.position.z);
+                            circleCenterToCalibrate.transform.position = controllerForCalibration.transform.position;
                             circleCenterToCalibrate.SetActive(true);
                             isCircleCenterSet = true;
                             buttonSetCircleCenter.SetActive(false);
