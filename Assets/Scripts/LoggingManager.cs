96beb976ca046f1afaef6c1965e5e76755ea0ce4
Christine Qiu <cqiu@kth.se>
2024-04-11 10:44:35 +0200

    Add logging

--- Assets/Scripts/LoggingManager.cs
+++ Assets/Scripts/LoggingManager.cs
@@ -11,14 +11,17 @@ public class LoggingManager : MonoBehaviour
     public GameObject signArrow;
     public CalibrationManager calibrationManager;
     private Vector3 playerToDest;
+    private Vector3 previousPlayerPosition;
     private float destinationThreshold = 2f;
     private bool isStart = false;
     private bool isFileWritten = false;
     private float startTime = 0f;
     private float endTime = 0f;
     private float duration = 0f;
+    private float distance = 0f;
+    private int countHitWall = 0;
     private string ROOT;
-    private string subjectname = "2022-10-04_13:00:00";
+    private string subjectname = "2024-04-11_13:00:00";
     private string savepath = "";
     private StreamWriter writer;
     // Start is called before the first frame update
@@ -28,8 +31,9 @@ public class LoggingManager : MonoBehaviour
         subjectname = System.DateTime.Now.ToString("yyyy-MM-dd_HH:mm:ss");
         savepath = Path.Combine(ROOT, subjectname + ".csv");
         writer = new StreamWriter(savepath, true);
-        string line = "Participant number, Completion time";
+        string line = "Participant number, Completion time (s), Distance (m), Count Hit Wall";
         writer.WriteLine(line);
+        previousPlayerPosition = player.position;
     }
 
     // Update is called once per frame
@@ -42,15 +46,26 @@ public class LoggingManager : MonoBehaviour
         }
         playerToDest = player.position - destination.position;
         Vector3 playerToDestXZ = new Vector3(playerToDest.x, 0f, playerToDest.z);
-        if(isStart && playerToDestXZ.magnitude < destinationThreshold){
-            isStart = false;
-            endTime = Time.time;
-            duration = endTime - startTime;
-            string line = "1," + duration.ToString("0.000");
-            writer.WriteLine(line);
-            writer.Close();
-            signArrow.SetActive(true);
-            isFileWritten = true;
+        if(isStart){
+            distance += new Vector3(player.position.x-previousPlayerPosition.x, 0f, player.position.z-previousPlayerPosition.z).magnitude;
+            previousPlayerPosition = player.position;
+            if(playerToDestXZ.magnitude < destinationThreshold){
+                isStart = false;
+                endTime = Time.time;
+                duration = endTime - startTime;
+                string line = "1," + duration.ToString("0.000") + "," + distance.ToString("0.000") + "," + countHitWall.ToString();
+                writer.WriteLine(line);
+                writer.Close();
+                signArrow.SetActive(true);
+                isFileWritten = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Building" && isStart){
+            countHitWall++;
         }
     }
 }
