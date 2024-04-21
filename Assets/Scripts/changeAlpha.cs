b211a8bb0705071afbb3218e336754ce43e3f4e4
Christine Qiu <cqiu@kth.se>
2024-04-21 23:41:31 +0200

    Try non-grid map

--- Assets/Scripts/changeAlpha.cs
+++ Assets/Scripts/changeAlpha.cs
@@ -7,22 +7,32 @@ public class changeAlpha : MonoBehaviour
     public float alpha;
     public CircleRedirection circleRedirection;
     public Transform circleCenter;
-    public Transform player;
+    public PlayerCollider playerCollider;
     private Bounds objectBounds;
 
     // Start is called before the first frame update
     void Start()
     {
-        Renderer renderer = GetComponent<Renderer>();
-        objectBounds = renderer.bounds;
+        // Renderer renderer = GetComponent<Renderer>();
+        // objectBounds = renderer.bounds;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerPos = new Vector3(player.position.x, circleCenter.position.y, player.position.z);
-        if (objectBounds.Contains(playerPos)){
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
             circleRedirection.alpha = alpha;
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
     }
 }
